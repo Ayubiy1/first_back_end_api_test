9bb0d870132fd3d7e15bde7d6d19f7c3c1d4a25f
Ayubiy1 <ayubiyurinboyev@gmail.com>
2024-11-21 22:44:11 +1100

    Your commit message here

--- routes/employer.js
+++ routes/employer.js
@@ -5,36 +5,29 @@ const router = Router()
 router.get("/", async (req, res) => {
     try {
         const employers = await pool.query('SELECT * FROM employer')
-
         res.status(200).json(employers.rows)
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
 })
-
 // add employer
-
 router.post('/add', async (req, res) => {
     try {
         const { name, salary, dagree, job_id } = req.body
         const newEmployer = await pool.query(`
       INSERT INTO employer (name, dagree, salary, job_id) VALUES ($1, $2, $3, $4) RETURNING *
       `, [name, dagree, salary, job_id])
-
         res.status(201).json(newEmployer.rows[0])
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
 })
-
-
 // update employer
 router.put("/:id", async (req, res) => {
     try {
         const { id } = req.params
         const { name, salary, dagree, job_id } = req.body
         const oldEmployerResult = await pool.query('SELECT * FROM employer WHERE id = $1', [id])
-
         // if (oldEmployerResult.rows.length === 0) {
         //     return res.status(404).json({ message: 'Employer not found' })
         // }
@@ -51,15 +44,11 @@ router.put("/:id", async (req, res) => {
             id
         ])
         res.status(201).json(updateEmployer.rows[0])
-
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
 })
-
-
 // delete employer
-
 router.delete('/:id', async (req, res) => {
     try {
         await pool.query('DELETE FROM employer WHERE id = $1', [req.params.id])
@@ -68,9 +57,7 @@ router.delete('/:id', async (req, res) => {
         res.status(500).json({ message: error.message })
     }
 })
-
 // get employer by id
-
 router.get('/:id', async (req, res) => {
     try {
         const employer = await pool.query(`
@@ -78,38 +65,10 @@ router.get('/:id', async (req, res) => {
       LEFT JOIN job ON job.id = employer.job_id 
       WHERE employer.id = $1
       `, [req.params.id])
-
         res.status(200).json(employer.rows[0])
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
 })
 
-
 module.exports = router
-
-{
-    // router.put("/:id", async (req, res) => {
-    //     console.log(req.body);
-
-    //     try {
-    //         const { id } = req.params
-    //         const { name, salary, dagree, job_id } = req.body
-    //         const oldEmployer = await pool.query('SELECT * FROM employer WHERE id = $1', [id])
-
-    //         const updatEmployer = await pool.query(`
-    //     UPDATE employer SET name = $1, dagree = $2, salary = $3, job_id = $4, WHERE id = $5 RETURNING *
-    //     `, [
-    //             name ? name : oldEmployer.rows[0].name,
-    //             dagree ? dagree : oldEmployer.rows[0].dagree,
-    //             salary ? salary : oldEmployer.rows[0].salary,
-    //             job_id ? job_id : oldEmployer.rows[0].job_id,
-    //             id])
-
-    //         res.status(201).json(updatEmployer.rows[0])
-
-    //     } catch (error) {
-    //         res.status(500).json({ message: error.message })
-    //     }
-    // })
-}
